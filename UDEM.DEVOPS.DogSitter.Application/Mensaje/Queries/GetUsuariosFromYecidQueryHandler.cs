67568368596bd62b3dc902b63012087840f066f8
FlorezZ1025 <sanaraflorez@gmail.com>
2026-04-14 13:53:48 -0500

    ✨ - Endpoint GET para probar la API de Yecid

---
+++ UDEM.DEVOPS.DogSitter.Application/Mensaje/Queries/GetUsuariosFromYecidQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using UDEM.DEVOPS.DogSitter.Domain.Ports;
+
+namespace UDEM.DEVOPS.DogSitter.Application.Mensaje.Queries
+{
+    public class GetUsuariosFromYecidQueryHandler(IMessageService messageService) : IRequestHandler<GetUsuariosFromYecidQuery, JsonNode>
+    {
+        public async Task<JsonNode> Handle(GetUsuariosFromYecidQuery request, CancellationToken cancellationToken)
+        {
+            return await messageService.ProbarApiYecid();
+        }
+    }
+}
---
+++ UDEM.DEVOPS.DogSitter.Domain/Dtos/RespuestaMensajeDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace UDEM.DEVOPS.DogSitter.Domain.Dtos
+{
+    public class RespuestaMensajeDto
+    {
+        private readonly JsonNode? EntidadDeSebas;
+        private readonly JsonNode? EntidadDeYecid;
+        private readonly JsonNode? EntidadDeSantiago;
+    }
+}
